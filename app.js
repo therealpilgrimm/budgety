99a7d293a58e3b754a87c5563f681bb04f4c2391
therealpilgrimm <patrickbrinca@gmail.com>
2020-04-07 12:12:17 +0200

    added clearFields Method to clear the input fields and change the focus back to the description

--- app.js
+++ app.js
@@ -93,6 +93,7 @@ var UIController = (function() {
   };
 
   return {
+
     getInput: function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // will be 'inc' or 'exp'
@@ -100,6 +101,7 @@ var UIController = (function() {
         value: document.querySelector(DOMstrings.inputValue).value
       }; 
     },
+
     addListItem: function(obj, type) {
       var html, newHtml, element;
       // creat HTML Strings with placeholder text
@@ -122,6 +124,20 @@ var UIController = (function() {
       // insert HTML in DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
+
+    clearFields: function () {
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index ,array){
+        current.value = "";
+      })
+
+      fieldsArr[0].focus();
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -173,10 +189,11 @@ var controller = (function(budgetCtrl, UICtrl) {
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
     UICtrl.addListItem(newItem, input.type);
+    // 4. Clear the fields
+    UICtrl.clearFields();
+    // 5. Calculate the budget
 
-    // 4. Calculate the budget
-
-    // 5. Display the budget on the UI
+    // 6. Display the budget on the UI
     
   };
 
