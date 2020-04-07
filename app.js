6cd77db6c2b75731cd1c1beaeec3678572087f12
therealpilgrimm <patrickbrinca@gmail.com>
2020-04-07 16:21:00 +0200

    added Delete functionality

--- app.js
+++ app.js
@@ -66,6 +66,23 @@ var budgetController =(function() {
       // Return the new element
       return newItem;
     },
+
+    deleteItem: function(type, id) {
+      var ids, index;
+
+      ids = data.allItems[type].map(function(current) {
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+
+    },
+
+
     calcBudget: function() {
 
       // calculate total inc and exp
@@ -126,7 +143,8 @@ var UIController = (function() {
     budgetLabel: '.budget__value',
     incomeLabel: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
-    percentageLabel: '.budget__expenses--percentage'
+    percentageLabel: '.budget__expenses--percentage',
+    container: '.container'
   };
 
   return {
@@ -145,11 +163,11 @@ var UIController = (function() {
       if(type === 'inc') {
         element = DOMstrings.incomeContainer;
 
-        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       } else if (type === 'exp') {
         element = DOMstrings.expensesContainer;
 
-        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
 
       // replace placeholder with data
@@ -160,6 +178,11 @@ var UIController = (function() {
 
       // insert HTML in DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+    },
+
+    deleteListItem: function(selectorID) {
+      var el = document.getElementById(selectorID);
+      el.parentNode.removeChild(el);
     },
 
     clearFields: function () {
@@ -208,13 +231,6 @@ var UIController = (function() {
 
 
 
-
-
-
-
-
-
-
 // GLOBAL APP CONTROLLER MODULE
 var controller = (function(budgetCtrl, UICtrl) {
   var setupEventListeners = function() {
@@ -227,7 +243,13 @@ var controller = (function(budgetCtrl, UICtrl) {
         ctrlAddItem();
       }
     });
+
+    document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem)
+
+
   };
+
+
 
   var updateBudget = function() {
 
@@ -239,6 +261,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     UICtrl.displayBudget(budget);
     
   };
+
+
   
   var ctrlAddItem = function() {
     var input, newItem;
@@ -257,6 +281,32 @@ var controller = (function(budgetCtrl, UICtrl) {
     }
 
   };
+
+
+
+  var ctrlDeleteItem = function (event) {
+    var itemID, splitID, type, ID;
+
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+
+    if (itemID) {
+      splitID = itemID.split('-');
+      type =splitID[0];
+      ID = splitID[1];
+    }
+
+    // 1. Delete the item from the data structure
+    budgetCtrl.deleteItem(type, parseInt(ID));
+    // 2. Delete the item from the UI
+    UICtrl.deleteListItem(itemID);
+    // 3. Update and show the new budget
+    updateBudget();
+
+
+  };
+
+
+
 
   return {
     init: function () {
